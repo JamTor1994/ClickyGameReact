ebbc61b8c3b59814c2c8f414cc2e01ecf92f2e36
James .T <torresjames48@yahoo.com>
2019-04-29 23:48:55 -0500

    more file organization

--- clickygame/src/components/cardBody.js
+++ clickygame/src/components/cardBody.js
@@ -1,24 +1,13 @@
 import React from "react";
-import Counter from "./clickCounter"
 import images from "../img.json"
 
 class Cardbody extends React.Component {
     constructor(props) {
         super(props)
         this.state = { names: [] }
-        // this.handleclick = this.handleclick.bind(this)
+ 
     }
-    // handleclick(event) {
-    //     console.log(event.target.name)
-    //     if (this.state.names.includes(event.target.name)) {
-    //         alert("clicked on ")
-    //         return
-    //     }
-    //     var newState = this.state.names;
-    //     newState.push(event.target.name)
-    //     this.setState({ names: newState })
-    //     console.log(this.state)
-    // }
+
     render() {
         return (
             <div className="card-container">
@@ -32,7 +21,7 @@ class Cardbody extends React.Component {
                                 {
                                     images.map(photo => {
                                         return (
-                                            <div key={photo.id} onClick={
+                                            <div className= "images" key={photo.id} onClick={
                                                 event => {
                                                     // this.handleclick(event);
                                                     this.props.handleIncrement(event)
